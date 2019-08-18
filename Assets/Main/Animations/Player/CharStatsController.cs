4e01399659ee0f881e14b5a50af861269c6d368d
Juan Barrera <31134624+JuanBarreraU@users.noreply.github.com>
2019-08-18 14:16:51 -0500

    Day03

---
+++ Assets/Main/Animations/Player/CharStatsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BattleRoyale
+{
+    [CreateAssetMenu(fileName = "Stats", menuName = "BattleRoyale/Char Stats")]
+    public class CharStatsController : ScriptableObject
+    {
+        public float fl_Speed = 6;
+        public float fl_RotationSpeed = 25;
+        public float fl_JumpForce = 25;
+        public float fl_MinAngle = -70;
+        public float fl_MaxAngle = 90;
+        public float fl_CameraSpeed = 24;
+
+        public float fl_CrouchHeightOffSet;
+        public float fl_CrouchPosOffSet;
+    }
+}
--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ namespace BattleRoyale
         private Transform tr_Transform;
         private Rigidbody rg_Rigidbody;
         private Animator anim_Animator;
+        CapsuleCollider col_Collider;
 
         public Transform tr_CameraShoulder;
         public Transform tr_CameraHolder;
@@ -18,11 +19,12 @@ namespace BattleRoyale
         private float fl_RotY = 0f;
         private float fl_RotX = 0f;
 
-        public float fl_Speed = 6;
-        public float fl_RotationSpeed = 25;
-        public float fl_MinAngle = -70;
-        public float fl_MaxAngle = 90;
-        public float fl_CameraSpeed = 24;
+        public CharStatsController _Stats;
+
+        public bool bl_OnGround = false;
+        public bool bl_Jumping = false;
+        public bool bl_Crouch = false;
+        public bool bl_Crouching = false;
 
         private Vector2 vec_MoveDelta;
         private Vector2 vec_MouseDelta;
@@ -35,6 +37,7 @@ namespace BattleRoyale
         {
             tr_Transform = this.transform;
             rg_Rigidbody = this.GetComponent<Rigidbody>();
+            col_Collider = GetComponent<CapsuleCollider>();
             anim_Animator = this.GetComponentInChildren<Animator>();
             tr_Cam = Camera.main.transform;
         }
@@ -57,6 +60,8 @@ namespace BattleRoyale
             float fl_DeltaZ = _Input.fl_Check("Vertical");
             float fl_MouseX = _Input.fl_Check("Mouse X");
             float fl_MouseY = _Input.fl_Check("Mouse Y");
+            bl_Jumping = _Input.bl_Check("Jump");
+            bl_Crouching = _Input.bl_Check("Crouch");
 
             vec_MoveDelta = new Vector2(fl_DeltaX, fl_DeltaZ);
             vec_MouseDelta = new Vector2(fl_MouseX, fl_MouseY);
@@ -66,36 +71,85 @@ namespace BattleRoyale
         //Move the character
         private void MoveController()
         {
-            Vector3 vec_Speed = rg_Rigidbody.velocity;
-
-            Vector3 vec_Side = fl_Speed * vec_MoveDelta.x * fl_DeltaT * tr_Transform.right;
-            Vector3 vec_Forward = fl_Speed * vec_MoveDelta.y * fl_DeltaT * tr_Transform.forward;
+            Vector3 vec_Side = _Stats.fl_Speed * vec_MoveDelta.x * fl_DeltaT * tr_Transform.right;
+            Vector3 vec_Forward = _Stats.fl_Speed * vec_MoveDelta.y * fl_DeltaT * tr_Transform.forward;
 
             Vector3 vec_EndSpeed = vec_Side + vec_Forward;
 
-            rg_Rigidbody.velocity = vec_EndSpeed;
+            RaycastHit hit;
+            bl_OnGround = Physics.Raycast(this.tr_Transform.position, -tr_Transform.up, out hit, .2f);
+            if(bl_OnGround)
+            {
+                if(bl_Crouching)
+                {
+                    OnCrouch();
+                }
+
+                if(bl_Jumping)
+                {
+                    if (bl_Crouch)
+                    {
+                        OnCrouch();
+                    }
+                    else
+                    {
+                        Jump();
+                    }
+                }
+
+                Vector3 vec_Speed = rg_Rigidbody.velocity;
+                vec_EndSpeed.y = vec_Speed.y;
+
+                rg_Rigidbody.velocity = vec_EndSpeed;
+            }
+            else
+            {
+                if(bl_Crouch)
+                {
+                    OnCrouch();
+                }
+            }
+
+
+        }
+
+        public void Jump()
+        {
+            rg_Rigidbody.AddForce(tr_Transform.up * _Stats.fl_JumpForce);
+        }
+
+        public void OnCrouch()
+        {
+            bl_Crouch = !bl_Crouch;
+            bl_Crouching = false;
+            float fl_Mult = (bl_Crouch ? 1 : -1);
+            col_Collider.center = col_Collider.center + new Vector3(0, _Stats.fl_CrouchPosOffSet, 0) * fl_Mult;
+            col_Collider.height += _Stats.fl_CrouchHeightOffSet * fl_Mult;
+            tr_CameraShoulder.position = tr_CameraShoulder.position + new Vector3(0, _Stats.fl_CrouchPosOffSet, 0) * fl_Mult;
         }
 
         //Move the camera
         private void CameraControl()
         {
-            fl_RotY += vec_MouseDelta.y * fl_DeltaT * fl_RotationSpeed;
-            float fl_XRot = vec_MouseDelta.x * fl_DeltaT * fl_RotationSpeed;
+            fl_RotY += vec_MouseDelta.y * fl_DeltaT * _Stats.fl_RotationSpeed;
+            float fl_XRot = vec_MouseDelta.x * fl_DeltaT * _Stats.fl_RotationSpeed;
 
             tr_Transform.Rotate(0, fl_XRot, 0);
 
-            fl_RotY = Mathf.Clamp(fl_RotY, fl_MinAngle, fl_MaxAngle);
+            fl_RotY = Mathf.Clamp(fl_RotY, _Stats.fl_MinAngle, _Stats.fl_MaxAngle);
 
             Quaternion qt_LocalRotation = Quaternion.Euler(-fl_RotY, 0, 0);
             tr_CameraShoulder.localRotation = qt_LocalRotation;
 
-            tr_Cam.position = Vector3.Lerp(tr_Cam.position, tr_CameraHolder.position, fl_CameraSpeed * fl_DeltaT);
-            tr_Cam.rotation = Quaternion.Lerp(tr_Cam.rotation, tr_CameraHolder.rotation, fl_CameraSpeed * fl_DeltaT);
+            tr_Cam.position = Vector3.Lerp(tr_Cam.position, tr_CameraHolder.position, _Stats.fl_CameraSpeed * fl_DeltaT);
+            tr_Cam.rotation = Quaternion.Lerp(tr_Cam.rotation, tr_CameraHolder.rotation, _Stats.fl_CameraSpeed * fl_DeltaT);
         }
 
         //Animate the character
         private void AnimControl()
         {
+            anim_Animator.SetBool("Ground", bl_OnGround);
+            anim_Animator.SetBool("Crouch", bl_Crouch);
             anim_Animator.SetFloat("X", vec_MoveDelta.x);
             anim_Animator.SetFloat("Y", vec_MoveDelta.y);
         }
